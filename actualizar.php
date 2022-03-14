e7a5cdb922a09f70d0611572c6c5cbfcc4e478b9
renancarrillo <renancarrillo@gmail.com>
2022-03-14 14:19:22 -0700

    Observacines y feedback de test. Quantumbit

--- actualizar.php
+++ actualizar.php
@@ -21,6 +21,8 @@ $row=mysqli_fetch_array($query)
         <link href="css/style.css" rel="stylesheet">
         <title>Actualizar</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <!-- Aqui los archivos estan apuntando mal entonces en servidor Linux no las encuentra (la 'i' es mayuscula)-->
         <link rel="stylesheet" href="interfasCliente/css/normalize.css">
         <link rel="stylesheet" href="interfasCliente/css/custom.css">
     </head>
